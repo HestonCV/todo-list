7b59f90af667a19b9fddfd27d5ce81fd8e7e04a6
Heston Vaughan <heston.cv@gmail.com>
2023-05-27 20:32:55 -0500

    add sidebar collapse style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./style.css";
+
+const collapseIcon = document.getElementById("collapse-icon");
+
+collapseIcon.addEventListener("click", () => {
+  const sideBar = document.querySelector(".content .left");
+  if (sideBar.classList.contains("collapsed")) {
+    console.log("test");
+    sideBar.classList.remove("collapsed");
+  } else {
+    sideBar.classList.add("collapsed");
+  }
+});
