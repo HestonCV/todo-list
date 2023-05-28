e34fa3a66a1db842832ad1eead2da89900a0f174
Heston Vaughan <heston.cv@gmail.com>
2023-05-28 02:33:31 -0500

    separate css file, add task styles, add completed tab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import "./style.css";
+import "./styles/main.css";
 
 const collapseIcon = document.getElementById("collapse-icon");
 
 collapseIcon.addEventListener("click", () => {
   const sideBar = document.querySelector(".content .left");
   if (sideBar.classList.contains("collapsed")) {
-    console.log("test");
     sideBar.classList.remove("collapsed");
   } else {
     sideBar.classList.add("collapsed");
